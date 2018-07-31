85c7c117aac8dc0ee172c44a4521762dc420f973
Rodney Cuthbertson <you@example.com>
2018-07-31 20:58:37 +0930

    Second batch

---
+++ searchHistory.py
@@ -0,0 +1,60 @@
+from __future__ import print_function
+import json
+import datetime
+import tzlocal
+import sys
+
+
+print('Opening file...')
+
+with open("E:\Location History\Takeout\LocationHistory.json", "r") as locationJSON:
+    dictionary = json.load(locationJSON)
+    locationJSON.close()
+    
+#show user starting message
+print('Finding dates within range')
+
+#show user size of file
+recordCount = len(dictionary['locations'])
+print('Number of records to process: ' + str(recordCount))
+
+recordsProcessed = 0
+recordsFound = 0
+newList = []
+
+for record in dictionary['locations']:
+    
+    #get local time zone        
+    localTimezone = tzlocal.get_localzone()
+
+    #convert timestamp to datetime
+    timestamp = datetime.datetime.fromtimestamp(float(record["timestampMs"])/1000, localTimezone) 
+
+    #define tax year dates
+    taxStartDate = datetime.datetime(2016,07,01,tzinfo=localTimezone) #.strftime('%a, %d %b %Y %H:%M:%S')
+    taxEndDate = datetime.datetime(2017,06,30,tzinfo=localTimezone)         
+
+    #check if in range
+    if timestamp > taxStartDate and timestamp < taxEndDate:
+        
+        #printout for user
+        recordsFound += 1
+        sys.stdout.write("\rNumber of records found: " + str(recordsFound))
+        sys.stdout.flush()
+
+        #push to new file
+        newList.append(record)
+        
+    recordsProcessed += 1  
+
+print("\nWriting new file...")
+newDic = {'locations' : []}
+newDic['locations'] = newList
+
+with open("finacialYear.json", 'w') as outfile:
+    json.dump(newDic, outfile)
+    outfile.close()
+    
+
+print('\nFinished')
+
---
+++ searchHistory_2.py
@@ -0,0 +1,50 @@
+from __future__ import print_function
+import json
+import datetime
+import tzlocal
+import sys
+import csv
+
+
+print('Opening file...')
+
+with open("E:\\Location History\\Takeout\\finacialYear.json", "r") as locationJSON:
+    dictionary = json.load(locationJSON)
+    locationJSON.close()
+   
+
+#show user size of file
+recordCount = len(dictionary['locations'])
+print('Number of records to process: ' + str(recordCount))
+
+#set up variables
+
+dateList = []
+recordList = []
+longTarget = 1385308020
+latTarget = -349373620
+accuracy = 1000
+print(dictionary['locations'][0].keys())
+
+#get local time zone        
+localTimezone = tzlocal.get_localzone()
+
+for record in dictionary['locations']:
+    
+    timestamp = datetime.datetime.fromtimestamp(float(record["timestampMs"])/1000, localTimezone)
+
+    if (timestamp.date() not in dateList) and \
+       (record['longitudeE7'] > longTarget - accuracy and record['longitudeE7'] < longTarget + accuracy) and \
+       (record['latitudeE7'] > latTarget - accuracy and record['latitudeE7'] > latTarget + accuracy):
+        print("adding reccord")
+        dateList.append(timestamp.date())
+        recordList.append(record)
+
+with open('dates.csv', 'wb') as dateWriter:
+    file = csv.writer(dateWriter, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for date in dateList:
+        file.writerow([date])
+    
+print(dateList)
+print('\nFinished')
+
